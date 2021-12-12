e9cd4b878d42aef76503066452cfdc8b7c9ee88b
Marina Magoche <nina.mags@gmail.com>
2021-12-12 23:58:16 +0000

    JS edits

--- script/script.js
+++ script/script.js
@@ -1,7 +1,4 @@
-//Welcome message on index page
-function welcomeMsg() {
-    alert("Welcome to KatMar Fashoin Gallery!")
-}
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 //Function for Google search when someone searches in a search bar.
 const f = document.getElementById('form');
@@ -20,67 +17,72 @@ f.addEventListener('submit', submitted);
 
 //Function Send a message Form /Contact Page
 function myMessage() {
-    event.preventDefault();
+  event.preventDefault();
 
-    var name = document.getElementById("userName").value;
-    var email = document.getElementById("userEmail").value;
-    var message = document.getElementById("userMessage").value;
-
+  var name = document.getElementById("userName").value;
+  var email = document.getElementById("userEmail").value;
+  var message = document.getElementById("userMessage").value;
+  if (email.match(mailformat)) {
     document.getElementById("userForm").innerHTML = name + ", thank you for your details. We will be in touch via " + email + " shortly.";
+    hide();
+  }
+  else {
+    alert("You have entered an invalid email address!");
+    return (false);
+  }
 
 }
 function hide() {
-    document.getElementById("userForm").style.display = "none";
+  document.getElementById("userForm").style.display = "none";
 }
 
 
 //Newsletter Form
 function hidden() {
-    var x = document.getElementById("hidden");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    }
-    else {
-        x.style.display = "block";
-    }
+  var x = document.getElementById("hidden");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+  }
 }
 function mySubscription() {
-    var email = document.getElementById("userEmail").value;
+  var email = document.getElementById("userEmail").value;
 
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.match(mailformat)) {
-        alert("Thank you for the subscription. We will send the latest news to " + email);
-        hide();
-    }
-    else {
-        alert("You have entered an invalid email address!");
-        return (false);
-    }
+  if (email.match(mailformat)) {
+    alert("Thank you for the subscription. We will send the latest news to " + email);
+    hide();
+  }
+  else {
+    alert("You have entered an invalid email address!");
+    return (false);
+  }
 
 }
 function hide() {
-    document.getElementById("userSub").innerHTML = "\u2713 You have subscribed";
+  document.getElementById("userSub").innerHTML = "\u2713 You have subscribed";
 }
 
 //Gallery page images
 function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
 }
 
 //Go to the Top Button 
 var mybutton = document.getElementById("topBtn");
 
 //Go to the Top Button-after 20px from the top the button will show
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -97,13 +99,13 @@ function topFunction() {
 }
 
 //jQuery Contact Page show phone,address and Opening Hours
-$( "#but1" ).click(function() {
-  $( "#show1" ).toggle( "slow" );
+$("#but1").click(function () {
+  $("#show1").toggle("slow");
 });
-$( "#but2" ).click(function() {
-  $( "#show2" ).toggle( "slow" );
+$("#but2").click(function () {
+  $("#show2").toggle("slow");
 });
-$( "#but3" ).click(function() {
-  $( "#show3" ).toggle( "slow" );
+$("#but3").click(function () {
+  $("#show3").toggle("slow");
 });
 
